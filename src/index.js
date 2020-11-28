d17be296642bf0532d5f9009b7494efbb614bfba
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-28 13:15:05 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 var http = require("http");
 
-const httpServer = http.createServer(function (req, res) {
+const httpServer = http.createServer(handleServer);
+function handleServer(req, res) {
   if (req.url === "/welcome") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("Welcome to Dominos!");
     res.end();
   } else if (req.url === "/contact") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.send({
-      phone: "18602100000",
-      email: "guestcaredominos@jublfood.com"
-    });
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
     res.end();
   } else {
-    res.send({
-      status: 404
-    });
-    res.end();
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404 Not Found");
   }
-});
+}
 httpServer.listen(8081);
 module.exports = httpServer;
